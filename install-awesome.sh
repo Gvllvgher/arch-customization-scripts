8d70014f1b9d12b1c7e5261c14e85fd1f35f39ec
Justin Gallagher <justingallagher@outlook.com>
2022-06-15 13:46:29 -0400

    Process

--- install-awesome.sh
+++ install-awesome.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
 pacman -Sy xorg-server xorg-xinit xterm awesome firefox xfce4-taskmanager xfce4-power-manager xfce4-settings scrot pcmanfm kitty nitrogen picom file-roller volumeicon dmenu numlockx --noconfirm > /dev/null
-echo “exec awesome” > ~/.xinitrc
+echo “exec awesome” > /home/justin/.config/.xinitrc
 
 mkdir /home/justin/.config  > /dev/null
 mkdir /home/justin/.config/awesome  > /dev/null
 git -C /temp clone --recurse-submodules --remote-submodules --depth 1 -j 2 https://github.com/lcpz/awesome-copycats.git
 mv -bv /temp/awesome-copycats/{*,.[^.]*} /home/justin/.config/awesome; rm -rf /temp/awesome-copycats
 chown -R justin:justin /home/justin/.config/awesome > /dev/null
+chown -R justin:justin /home/justin/.config > /dev/null
 
 cp /temp/arch-customization-scripts/rc.lua /home/justin/.config/awesome/rc.lua
 
-runuser -l justin 'yay -Sy lain-git --noconfirm'
+su justin<<'EOF'
+set -e
+yay -Sy lain-git --noconfirm
+exit
+EOF
+
+echo "installed awesomewm"
