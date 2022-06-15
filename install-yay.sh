4f788af522df00f6ef095616ebbabd9fced6028e
Justin Gallagher <justingallagher@outlook.com>
2022-06-15 09:57:50 -0400

    runuser

--- install-yay.sh
+++ install-yay.sh
@@ -3,6 +3,6 @@
 git clone https://aur.archlinux.org/yay-git.git /opt > /dev/null
 chown -R ${USER}:${USER} /opt/yay-git > /dev/null
 cd /opt/yay-git
-makepkg -si > /dev/null
-yay -Syu > /dev/null
+runuser -l justin 'makepkg -si' > /dev/null
+runuser -l justin 'yay -Syu' > /dev/null
 echo "installed and updated yay"
