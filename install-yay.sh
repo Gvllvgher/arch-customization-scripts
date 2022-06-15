52ed1b417529b91833bf35a9b47a167de24f97d1
Justin Gallagher <justingallagher@outlook.com>
2022-06-15 09:58:38 -0400

    hardcode username

--- install-yay.sh
+++ install-yay.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 git clone https://aur.archlinux.org/yay-git.git /opt > /dev/null
-chown -R ${USER}:${USER} /opt/yay-git > /dev/null
+chown -R justin:justin /opt/yay-git > /dev/null
 cd /opt/yay-git
 runuser -l justin 'makepkg -si' > /dev/null
 runuser -l justin 'yay -Syu' > /dev/null
