946e4afc477d79cde354215a64460e5b758288ef
Justin Gallagher <justingallagher@outlook.com>
2022-06-15 14:29:19 -0400

    update perms and .xinitrc

--- install-awesome.sh
+++ install-awesome.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 
-pacman -Sy xorg-server xorg-xinit xterm awesome firefox xfce4-taskmanager xfce4-power-manager xfce4-settings scrot pcmanfm kitty nitrogen picom file-roller volumeicon dmenu numlockx --noconfirm > /dev/null
-echo “exec awesome” > /home/justin/.config/.xinitrc
+pacman -Sy xorg-server xorg-xinit xterm awesome xorg-xclock firefox xfce4-taskmanager xfce4-power-manager xfce4-settings scrot pcmanfm kitty nitrogen picom file-roller volumeicon dmenu numlockx --noconfirm > /dev/null
+echo “exec awesome” > /home/justin/.xinitrc
+chown justin:justin /home/justin/.xinitrc
 
 mkdir /home/justin/.config  > /dev/null
 mkdir /home/justin/.config/awesome  > /dev/null
 git -C /temp clone --recurse-submodules --remote-submodules --depth 1 -j 2 https://github.com/lcpz/awesome-copycats.git
 mv -bv /temp/awesome-copycats/{*,.[^.]*} /home/justin/.config/awesome; rm -rf /temp/awesome-copycats
-chown -R justin:justin /home/justin/.config/awesome > /dev/null
-chown -R justin:justin /home/justin/.config > /dev/null
 
 cp /temp/arch-customization-scripts/rc.lua /home/justin/.config/awesome/rc.lua
 
+chown -R justin:justin /home/justin/.config/awesome > /dev/null
+chown -R justin:justin /home/justin/.config > /dev/null
+
 su justin<<'EOF'
 set -e
-yay -Sy lain-git --noconfirm
+yay -Sy lain-git ttf-google-fonts-git --noconfirm
 exit
 EOF
 
