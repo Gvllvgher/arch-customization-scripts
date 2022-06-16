9484f8936a8d5388f52c955be23cc24144efcc20
Justin Gallagher <justingallagher@outlook.com>
2022-06-16 09:57:07 -0400

    do not enable sddm service for debugging

--- install-sddm.sh
+++ install-sddm.sh
@@ -6,4 +6,4 @@ cp /temp/arch-customization-scripts/sddm/theme.conf /etc/sddm.conf.d/
 cp /temp/arch-customization-scripts/sddm/default.conf /usr/lib/sddm/sddm.conf.f/
 cp -r /temp/arch-customization-scripts/sddm/archcraft /usr/share/sddm/themes/
 
-systemctl enable sddm.service
+#systemctl enable sddm.service
