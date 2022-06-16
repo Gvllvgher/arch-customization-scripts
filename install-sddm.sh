2d96813ac5374218aba495c2959c3600f9cd8f03
Justin Gallagher <justingallagher@outlook.com>
2022-06-16 09:21:14 -0400

    enable sddm service

--- install-sddm.sh
+++ install-sddm.sh
@@ -5,3 +5,5 @@ pacman -Sy sddm --noconfirm
 cp /temp/arch-customization-scripts/sddm/theme.conf /etc/sddm.conf.d/
 cp /temp/arch-customization-scripts/sddm/default.conf /usr/lib/sddm/sddm.conf.f/
 cp -r /temp/arch-customization-scripts/sddm/archcraft /usr/share/sddm/themes/
+
+systemctl enable sddm.service
