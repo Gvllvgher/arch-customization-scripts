3cd757d527ae5dff014f8ab40aff3f5e6e3a823f
Justin Gallagher <justingallagher@outlook.com>
2022-06-16 13:03:15 -0400

    echo

--- install-chadwm.sh
+++ install-chadwm.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
-pacman -Sy dash imlib2 xorg-xsetroot hsetroot > /dev/null
+pacman -Sy dash imlib2 xorg-xsetroot hsetroot libx11 libxft libxinerama xorg-server xorg-xinit freetype2 fontconfig --noconfirm > /dev/null
 
 su justin<<EOF
-yay -Sy nerd-fonts-jetbrains-mono ttf-jetbrains-mono ttf-material-design-icons > /dev/null
+yay -Sy nerd-fonts-jetbrains-mono ttf-jetbrains-mono ttf-material-design-icons --noconfirm > /dev/null
 git clone https://github.com/siduck/chadwm --depth 1  ~/.config
 cd ~/.config/chadwm/chadwm
 mv eww ~/.config
 sudo make install
 exit
 EOF
+
+echo "installed chadwm"
 
 mkdir -p /usr/share/xsessions
 cp /temp/arch-customization-scripts/chadwm/chadwm.desktop /usr/share/xsessions/
