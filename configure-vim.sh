59ca3d1e3a3f659a74e80284203780ec284cd1ba
Justin Gallagher <justingallagher@outlook.com>
2022-06-17 13:40:35 -0400

    fix EOF not expaning variables

--- configure-vim.sh
+++ configure-vim.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}";    )" &> /dev/null && pwd 2> /dev/null;    )";
 
-su justin<<'EOF'
+su justin<<EOF
 set -e
 cp -r $SCRIPT_DIR/vim/.vim_runtime ~/
 cp $SCRIPT_DIR/vim/.vimrc ~/
