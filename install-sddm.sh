daa3305179ee5009cdeedfa12df6d111f072b5df
Justin Gallagher <justingallagher@outlook.com>
2022-06-16 21:30:59 -0400

    script_dir

--- install-sddm.sh
+++ install-sddm.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}";   )" &> /dev/null && pwd 2> /dev/null;   )";
+
 pacman -Sy sddm qt5-quickcontrols2 qt5-graphicaleffects --noconfirm > /dev/null
 
 mkdir /etc/sddm.conf.d
 
-cp /temp/arch-customization-scripts/sddm/theme.conf /etc/sddm.conf.d/
-cp /temp/arch-customization-scripts/sddm/default.conf /usr/lib/sddm/sddm.conf.d/
-cp -r /temp/arch-customization-scripts/sddm/archcraft /usr/share/sddm/themes/
+cp $SCRIPT_DIR/sddm/theme.conf /etc/sddm.conf.d/
+cp $SCRIPT_DIR/sddm/default.conf /usr/lib/sddm/sddm.conf.d/
+cp -r $SCRIPT_DIR/sddm/archcraft /usr/share/sddm/themes/
 
 systemctl enable sddm.service
