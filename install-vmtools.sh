80f5aec48ea24e27d7a29490f66b0ff85033a3dd
Justin Gallagher <justingallagher@outlook.com>
2022-06-17 21:27:06 -0400

    enable vmware-vmblock-fuse.service

--- install-vmtools.sh
+++ install-vmtools.sh
@@ -5,3 +5,4 @@ pacman -Su open-vm-tools xf86-input-vmmouse xf86-video-vmware mesa gtk2 gtkmm gt
 echo "needs_root_rights=yes" >> /etc/X11/Xwrapper.config
 
 systemctl enable vmtoolsd
+systemctl enable vmware-vmblock-fuse.service
