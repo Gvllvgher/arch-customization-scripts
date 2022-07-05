243a34fd3a5221a2d1d7962189cb6a6fb24dc597
Justin Gallagher <justingallagher@outlook.com>
2022-07-05 09:45:55 -0400

    username for install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,4 +1,27 @@
 #! /bin/bash
 
+# Parameters
+while getopts ':u:' opt; do
+    case $opt in
+        u)
+            LOCAL_USER=${OPTARG}
+            echo "install-zsh.sh: LOCAL_USER set to: ${OPTARG}"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$LOCAL_USER" ]]; then
+    echo "install-zsh.sh: Use paramter -u to specify a username."
+    exit 1
+fi
+
 pacman -Sy zsh --noconfirm > /dev/null
-chsh -s $(which zsh) justin
+chsh -s $(which zsh) $LOCAL_USER
