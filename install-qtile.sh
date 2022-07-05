4ac1c9817428f87a7cffae9a4f000828ca1b8056
Justin Gallagher <justingallagher@outlook.com>
2022-07-05 15:23:17 -0400

    root context

--- install-qtile.sh
+++ install-qtile.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# Parameters
+while getopts ':u:' opt; do
+    case $opt in
+        u)
+            LOCAL_USER=${OPTARG}
+            echo "install-qtile.sh: LOCAL_USER set to: ${OPTARG}"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$LOCAL_USER" ]]; then
+    echo "install-qtile.sh: Use paramter -u to specify a username."
+    exit 1
+fi
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}";     )" &> /dev/null && pwd 2> /dev/null;     )";
 
 # Install Dependencies
@@ -9,8 +32,9 @@ pacman -S xorg-server xorg-xinit xterm xfce4-terminal lightdm-gtk-greeter xfce4-
 pacman -S qtile lightdm
 
 # Enable the lightdm service for autostart
-sudo systemctl enable lightdm
+systemctl enable lightdm
 
 # Copy default configuration
-mkdir -p ~/.config/qtile
-cp /usr/share/doc/qtile/default_config.py ~/.config/qtile/config.py
+mkdir -p /home/$LOCAL_USER/.config/qtile
+cp /usr/share/doc/qtile/default_config.py /home/$LOCAL_USER/.config/qtile/config.py
+chown -R $LOCAL_USER:$LOCAL_USER /home/$LOCAL_USER/.config
