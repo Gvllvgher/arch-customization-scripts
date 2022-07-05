953dcd771d2da9fd66ee6d319f0e1e841688a544
Justin Gallagher <justingallagher@outlook.com>
2022-07-05 09:47:03 -0400

    username for install-omz.sh

--- install-omz.sh
+++ install-omz.sh
@@ -1,6 +1,29 @@
 #! /bin/bash
 
-su justin<<'EOF'
+# Parameters
+while getopts ':u:' opt; do
+    case $opt in
+        u)
+            LOCAL_USER=${OPTARG}
+            echo "install-omz.sh: LOCAL_USER set to: ${OPTARG}"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$LOCAL_USER" ]]; then
+    echo "install-omz.sh: Use paramter -u to specify a username."
+    exit 1
+fi
+
+su $LOCAL_USER<<'EOF'
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 exit
 EOF
