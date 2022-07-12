111ccd7ded0ec565ae47c9430831e6e7fdc95f2f
Justin Gallagher <justingallagher@outlook.com>
2022-07-12 19:16:44 -0400

    dotfiles installer

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,8 +4,7 @@ dotfilesUrl="https://github.com/Gvllvgher/dotfiles"
 
 su $LOCAL_USER<<EOF
 set -e
-mkdir -p ~/git
-cd ~/git
-git clone ${dotfilesUrl} > /dev/null
-/dotfiles/link.sh
+mkdir -p /home/$LOCAL_USER/git
+git -c /home/$LOCAL_USER/git clone ${dotfilesUrl} > /dev/null
+/home/$LOCAL_USER/dotfiles/link.sh
 EOF
