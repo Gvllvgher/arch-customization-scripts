31b3cab1218723d2ee604f55cfb4ed455b1211f3
Justin Gallagher <justingallagher@outlook.com>
2022-07-13 15:34:34 -0400

    app installer

--- install-apps.sh
+++ install-apps.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-pacmanApps=(
+pacmanApps=( \
     "exa
+    firefox
+    flameshot
+    starship
     alacritty
     thefuck" \
-    )
+)
 
-pacman -S ${pacmanApps} --noconfirm > /dev/null 
+yayApps=( \
+    "betterlockscreen" \
+)
+
+appScripts=( \
+    "$SCRIPT_DIR/apps/install-zsh.sh" \
+    "$SCIPT_DIR/apps/install-yay.sh " \
+)
+
+pacman -S ${pacmanApps} --noconfirm > /dev/null
+
+su $LOCAL_USER << EOF
+set -e
+yay -S ${yayApps} --noconfirm > /dev/null
+exit
+EOF
+
+for script in ${appScripts[@]}; do
+    chmod +x $script
+    $script
+done
