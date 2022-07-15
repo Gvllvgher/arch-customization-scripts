51b8a904749e2d51601e7e26fee19f4c7b85e1ee
Justin Gallagher <justingallagher@outlook.com>
2022-07-15 14:29:11 -0400

    acpi stuff

--- configure-acpid.sh
+++ configure-acpid.sh
@@ -9,10 +9,6 @@ systemctl enable acpid &> /dev/null
 # Start
 systemctl start acpid &> /dev/null
 
-sed -i "s#^logger "'"LID opened"'"$/{$!{ N
-		;;#logger "'"LID opened"'"
-                DISPLAY=:0.0 su justin -c "'"etc/acpi/dm-multimonitor.sh"'"
-		;;#g" /etc/acpi/handler.sh
+sed -i "s#logger 'LID closed'#logger 'LID closed' \&\& DISPLAY=:0.0 su justin -c 'etc/acpi/dm-multimonitor.sh'#g" /etc/acpi/handler.sh
 
-
-        sed '/^a test$/{$!{N;s/^a test\nPlease do not$/not a test\nBe/;ty;P;D;:y}}' alpha.txt
+sed -i "s#logger 'LID opened'#logger 'LID opened' \&\& DISPLAY=:0.0 su justin -c 'etc/acpi/dm-multimonitor.sh'#g" /etc/acpi/handler.sh
